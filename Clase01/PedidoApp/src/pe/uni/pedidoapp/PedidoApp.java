60b322052c749c80f9c831b0e5b765715a13fddf
Gustavo Coronel <gcoronelc@gmail.com>
2020-02-05 11:45:46 -0500

    Clase 01

---
+++ Clase01/PedidoApp/src/pe/uni/pedidoapp/PedidoApp.java
@@ -0,0 +1,9 @@
+package pe.uni.pedidoapp;
+
+public class PedidoApp {
+
+  public static void main(String[] args) {
+    System.out.println("Hola desde Java.");
+  }
+
+}
---
+++ Clase01/PedidoApp/src/pe/uni/pedidoapp/prueba/Prueba01.java
@@ -0,0 +1,27 @@
+package pe.uni.pedidoapp.prueba;
+
+import java.util.Scanner;
+import pe.uni.pedidoapp.service.PedidoService;
+
+
+public class Prueba01 {
+
+  public static void main(String[] args) {
+    // Variables
+    double importe, impuesto, total;
+    Scanner scanner = new Scanner(System.in);
+    // Datos
+    System.out.print("Importe: ");
+    importe = scanner.nextDouble();
+    // Proceso
+    PedidoService service = new PedidoService();
+    impuesto = service.calcImpuesto(importe);
+    total = service.calcTotal(importe);
+    // Reporte
+    System.out.println("REPORTE");
+    System.out.println("Impuesto: " + impuesto);
+    System.out.println("Total: " + total);
+  }
+
+  
+}
