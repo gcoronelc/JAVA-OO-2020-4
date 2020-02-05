c276087b026f484aefc63dee66f840be077a75bd
Gustavo Coronel <gcoronelc@gmail.com>
2020-02-05 13:40:17 -0500

    Clase 01

---
+++ Clase01/Pasaca-CalculadoraBasica/src/calculadorabasica/operaciones/operar.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorabasica.operaciones;
+
+/**
+ *
+ * @author Alumno
+ */
+public class operar {
+    public double suma(double numero1,double numero2)
+    {
+        double respuesta;
+     respuesta=numero1+numero2;
+     return respuesta;
+    }
+    public double resta(double numero1,double numero2)
+    {
+        double respuesta;
+     respuesta=numero1-numero2;
+     return respuesta;
+    }
+    public double multiplicacion(double numero1,double numero2)
+    {
+        double respuesta;
+     respuesta=numero1*numero2;
+     return respuesta;
+    }
+    public double division(double numero1,double numero2)
+    {
+        double respuesta;
+     respuesta=numero1/numero2;
+     return respuesta;
+    }
+}
